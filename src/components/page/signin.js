855878bc9e5953e2899d378de117e981589219a1
Ethen1264 <jxorhks86909763@gmail.com>
2023-12-07 16:15:14 +0900

    로그인 페이지 시각적인 요소 편집

--- src/components/page/signin.js
+++ src/components/page/signin.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { BlackButton } from '../public/BlackButton';
 import { Link } from 'react-router-dom';
@@ -167,22 +168,95 @@ export const ErrorText = styled.p`
   margin: 0;
   margin-top: 8px;
 `;
+export const ErrorText2 = styled.p`
+  font-family: 'Pretendard';
+  color: rgb(232, 124, 3);
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  margin: 0;
+  margin-top: 8px;
+  padding-bottom: 80px;
+`;
+
+const User = {
+  email: 'test@gmail.com',
+  password: 'test1234!!!'
+}
+
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailValid, setEmailValid] = useState(false)
+  const [passwordValid,setPasswordValid] = useState(false)
+  const [notAllow, setNotAllow] = useState (true)
+  const [changePw, setChangePw] = useState('password');
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+    const regex =  /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if(regex.test(email)) {
+      setEmailValid(true);
+    } else{
+      setEmailValid(false)
+    }
+  }
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+    const regex = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+    if(regex.test(password)){
+      setPasswordValid(true)
+    }else{
+      setPasswordValid(false)
+    }
+  }
+  const onClickConfirmButton = (e) => {
+    if (email === User.email && password === User.password) {
+      alert('로그인 되었습니다');
+    }
+    else alert('등록되지 않는 회원입니다');
+  };
+  const checkPassword = (e) =>{
+    if(changePw === 'password')
+    {
+      setChangePw('text');
+    }
+    else 
+    setChangePw('password');
+  }
+  useEffect(() => {
+    if(emailValid&&passwordValid){
+      setNotAllow(false)
+      return
+    }
+    setNotAllow(true)
+  }, [emailValid, passwordValid])
   return (
     <BodyContainer>
       <LoginContainer>
         <Title>Login ToU</Title>
         <EmailContainer>
           <Text>사용자 이메일</Text>
-          <Input></Input>
-          <ErrorText>잘못된 형식의 이메일 입니다.</ErrorText>
+          <Input
+          type='text'
+          value={email}
+          onChange={handleEmail} />
+          <ErrorText>
+          {!emailValid && email.length > 0 && <div>올바른 이메일을 입력해주세요.</div>}
+          </ErrorText>
         </EmailContainer>
         <PasswordContainer>
           <Text>비밀번호</Text>
-          <Input type="password" maxlength="36"></Input>
-          <CheckPassword>
+          <Input
+            type={changePw}
+            maxlength="16"
+            value={password}
+            onChange={handlePassword}
+          ></Input>
+          <CheckPassword onClick={checkPassword}>
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" viewBox="0 0 22 20" fill="none">
-              <path
+              <path 
                 d="M2.97996 6.223C2.04442 7.32718 1.34778 8.6132 0.93396 10C2.22596 14.338 6.24396 17.5 11 17.5C11.993 17.5 12.953 17.362 13.863 17.105M5.22796 4.228C6.94058 3.09781 8.94804 2.49682 11 2.5C15.756 2.5 19.773 5.662 21.065 9.998C20.3568 12.3673 18.8369 14.4115 16.772 15.772M5.22796 4.228L1.99996 1M5.22796 4.228L8.87796 7.878M16.772 15.772L20 19M16.772 15.772L13.122 12.122C13.4006 11.8434 13.6216 11.5127 13.7723 11.1486C13.9231 10.7846 14.0007 10.3945 14.0007 10.0005C14.0007 9.6065 13.9231 9.21636 13.7723 8.85235C13.6216 8.48834 13.4006 8.1576 13.122 7.879C12.8434 7.6004 12.5126 7.3794 12.1486 7.22863C11.7846 7.07785 11.3945 7.00025 11.0005 7.00025C10.6065 7.00025 10.2163 7.07785 9.85231 7.22863C9.4883 7.3794 9.15756 7.6004 8.87896 7.879M13.121 12.121L8.87996 7.88"
                 stroke="#999999"
                 stroke-width="1.5"
@@ -191,21 +265,23 @@ export default function Login() {
               />
             </svg>
           </CheckPassword>
-          <ErrorText>비밀번호 - 영어,숫자,특수문자를 각각 하나 이상 포함한 8자 이상 36자 이하 형식을 맞춰주세요</ErrorText>
+          <ErrorText2>
+          {!passwordValid && password.length > 0 && <div>비밀번호는 8~16자 사이여야 합니다.</div>}
+          </ErrorText2>
         </PasswordContainer>
-        <CheckBoxContainer>
+        {/* <CheckBoxContainer>
           <CheckBox type="checkbox"></CheckBox>
           <CheckBoxText>내 정보 기억하기</CheckBoxText>
-        </CheckBoxContainer>
+        </CheckBoxContainer> */}
         <Link to="/" style={{ textDecoration: 'none' }}>
-          <BlackButton>로그인하기</BlackButton>
+          <BlackButton onClick={onClickConfirmButton}  disabled={notAllow}>로그인하기</BlackButton>
         </Link>
-        <Link to="/ChangePassword" style={{ textDecoration: 'none' }}>
+        {/* <Link to="/ChangePassword" style={{ textDecoration: 'none' }}>
           <ForgetPassword>비밀번호를 잊으셨나요?</ForgetPassword>
         </Link>
         <Link to="/Signup" style={{ textDecoration: 'none' }}>
           <ForgetPassword2>계정이 없나요? ToU에 가입하기</ForgetPassword2>
-        </Link>
+        </Link> */}
       </LoginContainer>
     </BodyContainer>
   );
